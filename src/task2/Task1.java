0fc8c6fac1ec32343628f6dbb2b72c1f00f1dc1c
SHREKWAZOVSKI <iamdunock@gmail.com>
2024-01-10 18:35:52 +0200

    Initial commit

---
+++ src/task2/Task1.java
@@ -0,0 +1,23 @@
+package task2;
+
+public class Task1 {
+        public static void main(String[] args) {
+        /* Coздать две переменные типа int:
+        int a = 7;
+        int b = 9;
+        Поменять между собой значения переменных a и b.
+        Создавать новые переменные нельзя.
+        Можно применять только арифметические действия к перемнным a и b.
+        Вывести в консоль значения переменных a и b после их переопределения. */
+
+            int a = 7;
+            int b = 9;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            System.out.println(a);
+            System.out.println(b);
+        }
+    }
+
