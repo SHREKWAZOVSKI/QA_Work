45090f4b6cd5b2283becea55a7f254542c6a0ac7
SHREKWAZOVSKI <iamdunock@gmail.com>
2024-01-22 20:43:52 +0200

    Task6 added

---
+++ src/task6/Task6.java
@@ -0,0 +1,30 @@
+package task6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = random.nextInt(0,100);
+        int userNumber;
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Let the game begin!");
+        boolean endGame = false;
+        while (endGame==false){
+            System.out.println("Enter the number from 0 to 100");
+            userNumber=scanner.nextInt();
+            if (userNumber<randomNumber){
+                System.out.println("Your number is too small. Please, try again.");
+            }
+            else if (userNumber>randomNumber) {
+                System.out.println("Your number is too big. Please, try again");
+            }
+            else {
+                System.out.println("Congratulations, " + name + "!");
+            }
+        }
+    }
+}
